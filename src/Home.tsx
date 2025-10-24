118a33ec23391ac661c650c13924c8d3e2ec042d
Ygor Henrique <ygorlima.dev06@gmail.com>
2025-10-24 16:27:10 -0300

    conseguindo iterar os elementos dos países a partir do map

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,13 +1,28 @@
 import Header from "./components/Header";
 import Main from "./components/Main";
 import CountriesContainer from "./components/CountriesContainer";
+import Countrie from "./components/Countrie";
+import dados from './api/data.json'
+
 function Home() {
   return (
     <div>
       <Header/>
       <Main>
         <CountriesContainer>
-          <div>Card</div>
+          {
+            dados.map(({flag,name,population,region,capital})=>{
+              return(
+                <Countrie 
+                  flag={flag}
+                  name={name}
+                  population={population}
+                  region={region}
+                  capital={capital}
+                />
+              )
+            })
+          }
         </CountriesContainer>
       </Main>
     </div>
--- src/components/Countrie.tsx
+++ src/components/Countrie.tsx
@@ -1,7 +1,31 @@
-export default function Countrie(){
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+interface propsCountrie{
+    /*initials informations*/
+
+    flag:any;
+    name:string;
+    population:number;
+    region:string;
+    capital:any;
+
+    /*details informations
+        native_name:string;
+        sub_region:string;
+        top_level_domain:string;
+        currencies:string;
+        languages:string;    
+    */
+}
+
+export default function Countrie(props:propsCountrie){
     return(
-        <>
-        
-        </>
+        <div>
+            <img src={props.flag} alt={props.name} className="img-flag" />
+            <h3>{props.name}</h3>
+            <p>Population: {props.population}</p>
+            <p>Region: {props.region}</p>
+            <p>Capital: {props.capital}</p>
+        </div>
     )
 }
